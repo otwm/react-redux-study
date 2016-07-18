43b20d25f88df86630e8a32d52b4f26a66fa2c8f
otwm <otwmoo@gmail.com>
2016-07-18 18:05:11 +0900

    전체 테스트 추가

--- counter/test/components/Counter.spec.js
+++ counter/test/components/Counter.spec.js
@@ -34,4 +34,44 @@ describe('Counter component', () => {
         const {p} = setup()
         expect(p.text()).toMatch(/^Clicked: 0 times/)
     })
+
+    it('첫번째 버튼은 증가 함수 호출', ()=> {
+        const {buttons, actions} = setup()
+        buttons.at(0).simulate('click')
+        expect(actions.onIncrement).toHaveBeenCalled()
+    })
+
+    it('두번째 버튼은 감소 함수 호출', ()=> {
+        const {buttons, actions} = setup()
+        buttons.at(1).simulate('click')
+        expect(actions.onDecrement).toHaveBeenCalled()
+    })
+
+    it('세번째 버튼은 짝수에서는 증가하면 안됨', ()=> {
+        const {buttons, actions} = setup(40)
+        buttons.at(2).simulate('click')
+        expect(actions.onIncrement).toNotHaveBeenCalled()
+    })
+
+    it('세번째 버튼은 홀수에서는 증가해야됨', ()=> {
+        const {buttons, actions} = setup(41)
+        buttons.at(2).simulate('click')
+        expect(actions.onIncrement).toHaveBeenCalled()
+    })
+
+    it('세번째 버튼은 홀수에서는 증가해야됨(음수)', ()=> {
+        const {buttons, actions} = setup(-41)
+        buttons.at(2).simulate('click')
+        expect(actions.onIncrement).toHaveBeenCalled()
+    })
+
+    it('네번째 버튼은 일초 후 증가', (done)=> {
+        const {buttons, actions} = setup(-41)
+        buttons.at(3).simulate('click')
+        setTimeout(()=> {
+            expect(actions.onIncrement).toHaveBeenCalled()
+            done()
+        }, 1000)
+
+    })
 })
