3d80918d65100bd2c9ec589563ab66cafba18ead
otwm <otwmoo@gmail.com>
2016-07-18 17:29:52 +0900

    컴퍼넌트 구현 글자 확인 통과

--- counter/components/Counter.js
+++ counter/components/Counter.js
@@ -1,5 +1,14 @@
 import React, {Component, PropTypes} from 'react'
 
 class Counter extends Component {
-
+    render() {
+        const {value, onIncrement, onDecrement} = this.props
+        return (
+            <p>
+                Clicked: {value} times
+            </p>
+        )
+    }
 }
+
+export default Counter;
