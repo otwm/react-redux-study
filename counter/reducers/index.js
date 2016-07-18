daeef4b5dc9124cabc6efcc86bd2f37dc297a538
otwm <otwmoo@gmail.com>
2016-07-18 15:22:53 +0900

    리듀서 테스트

---
+++ counter/reducers/index.js
@@ -0,0 +1,3 @@
+export default function count(state = 0, action) {
+
+}
---
+++ counter/test/reducers/counter.spec.js
@@ -0,0 +1,21 @@
+import expect from 'expect';
+import counter from '../../reducers';
+describe('reducers', () => {
+    describe('counter', () => {
+        it('초기값 설정', () => {
+            expect(counter(undefined, {})).toBe(0)
+        })
+
+        it('증가 테스트', () => {
+            expect(counter(1, {type: 'INCREMENT'})).toBe(2)
+        })
+
+        it('감소 테스트', () => {
+            expect(counter(3, {type: 'DECREMENT'})).toBe(2)
+        })
+
+        it('이상한 상태는 무시', () => {
+            expect(counter(1, {type: 'unknown'})).toBe(1)
+        })
+    })
+})
